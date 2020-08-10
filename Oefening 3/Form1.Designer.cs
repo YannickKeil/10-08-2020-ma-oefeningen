198dbf9831885f8a291b573ecf0fb352de0b8c10
yannick keil <keilyannick@hotmail.com>
2020-08-10 13:28:20 +0200

    oefening 3 klaar

---
+++ Oefening 3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Oefening_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLijst = new System.Windows.Forms.ListBox();
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbLijst
+            // 
+            this.lbLijst.FormattingEnabled = true;
+            this.lbLijst.ItemHeight = 20;
+            this.lbLijst.Location = new System.Drawing.Point(116, 62);
+            this.lbLijst.Name = "lbLijst";
+            this.lbLijst.Size = new System.Drawing.Size(120, 84);
+            this.lbLijst.TabIndex = 0;
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.Location = new System.Drawing.Point(136, 182);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(100, 26);
+            this.txtSearchText.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(298, 171);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 48);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchText);
+            this.Controls.Add(this.lbLijst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLijst;
+        private System.Windows.Forms.TextBox txtSearchText;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Oefening 3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oefening_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static List<string> naamLijst = new List<string>();
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lbLijst.Items.Clear();
+            foreach (var item in naamLijst)
+            {
+                if (item.ToLower().Contains(txtSearchText.Text.ToLower()))
+                {
+                lbLijst.Items.Add(item);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            naamLijst.Add("An");
+            naamLijst.Add("Bart");
+            naamLijst.Add("Cedric");
+            naamLijst.Add("Dieter");
+            naamLijst.Add("Evert");
+            naamLijst.Add("filip");
+            naamLijst.Add("Gert");
+            naamLijst.Add("Henk");
+            foreach (var item in naamLijst)
+            {
+                lbLijst.Items.Add(item);
+            }
+        }
+    }
+}
