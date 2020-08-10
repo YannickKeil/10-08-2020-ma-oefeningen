06bb5e10781f110189001b43cc5908bab8af18e6
yannick keil <keilyannick@hotmail.com>
2020-08-10 14:36:41 +0200

    oefening 4

---
+++ Oefening 4/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Oefening_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbVoornamen = new System.Windows.Forms.ComboBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbVoornamen
+            // 
+            this.cbVoornamen.FormattingEnabled = true;
+            this.cbVoornamen.Location = new System.Drawing.Point(72, 73);
+            this.cbVoornamen.Name = "cbVoornamen";
+            this.cbVoornamen.Size = new System.Drawing.Size(121, 28);
+            this.cbVoornamen.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(304, 65);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 42);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "keuzen";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.cbVoornamen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbVoornamen;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
+
---
+++ Oefening 4/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_4
+{
+    public class User
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+
+        public User(string voornaam, string achternaam)
+        {
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+        }
+        
+    }
+}
