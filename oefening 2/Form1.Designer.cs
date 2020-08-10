c8d8f8a59e3d9a5ad473f67820e5f8928632800c
yannick keil <keilyannick@hotmail.com>
2020-08-10 13:10:25 +0200

    oefening 2 (iets was foutgelopen heb oefening 1 en 2 opnieuw moeten maken)

---
+++ oefening 2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace oefening_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLijst = new System.Windows.Forms.ComboBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbLijst
+            // 
+            this.cbLijst.FormattingEnabled = true;
+            this.cbLijst.Location = new System.Drawing.Point(108, 71);
+            this.cbLijst.Name = "cbLijst";
+            this.cbLijst.Size = new System.Drawing.Size(121, 28);
+            this.cbLijst.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(267, 71);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 52);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.cbLijst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbLijst;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ oefening 2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oefening_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cbLijst.SelectedItem.ToString());
+            cbLijst.Items.RemoveAt(cbLijst.SelectedIndex);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbLijst.Items.Add("Martijn");
+            cbLijst.Items.Add("Benjamin");
+            cbLijst.Items.Add("Koen");
+            cbLijst.Items.Add("Antal");
+            cbLijst.Items.Add("Yannick");
+            cbLijst.Items.Add("Latha");
+            cbLijst.Items.Add("Jonas");
+            cbLijst.Items.Add("Machiel");
+        }
+    }
+}
